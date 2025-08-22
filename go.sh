8f3b11559f450fef501804c1943c299c29bd93ae
Paul Wilkinson <cremoni@gmail.com>
2025-08-22 15:08:45 -0700

    add IMAGE_TAG to go.sh

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
+export IMAGE_TAG=0.4.11
 ./start.sh -s "https://sv.sv-2.dev.global.canton.network.digitalasset.com" -o "eyJzcG9uc29yaW5nU3YiOiJEaWdpdGFsLUFzc2V0LTI6OjEyMjBlNWEyOWQzOTQ5Y2Q4N2UyZGY5NzU1M2Y5MTUxZWVhYjZiNDk0OWE1ZmY3YTU4ODZiZDc0YjU5YzljYzk4MmYyIiwic2VjcmV0IjoienZ4c2ZPWktnWGgyYlZ1WkJYT2JEYS9rSHNoOTdLekc3dzJoLzBVRkRMWT0ifQ==" -p "dserv-validator-1" -m "0" -w
-
