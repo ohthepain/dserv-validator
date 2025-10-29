d1ba8790769725421ce05e224a6422d27a3983a9
Paul Wilkinson <cremoni@gmail.com>
2025-10-29 08:58:50 +0100

    console output for fetching validator onboarding secrets

--- config/validator/entrypoint.sh
+++ config/validator/entrypoint.sh
@@ -5,13 +5,15 @@ set -eo pipefail
 
 # fetch onboarding secret from super-validator if none is provided. Only works in LocalNet and DevNet
 if [[ -z "${APP_PROVIDER_VALIDATOR_ONBOARDING_SECRET:-}" ]]; then
-    echo "No onboarding secret provided. Attempting to fetch from super-validator via $ONBOARDING_SECRET_URL..."
+    echo "No provider onboarding secret provided. Attempting to fetch from super-validator via $ONBOARDING_SECRET_URL..."
     export APP_PROVIDER_VALIDATOR_ONBOARDING_SECRET="$(curl -sfL -X POST "$ONBOARDING_SECRET_URL")"
+    echo "Fetched provider onboarding secret: $APP_PROVIDER_VALIDATOR_ONBOARDING_SECRET"
 fi
 
 if [[ -z "${APP_USER_VALIDATOR_ONBOARDING_SECRET:-}" ]]; then
-    echo "No onboarding secret provided. Attempting to fetch from super-validator via $ONBOARDING_SECRET_URL..."
+    echo "No user onboarding secret provided. Attempting to fetch from super-validator via $ONBOARDING_SECRET_URL..."
     export APP_USER_VALIDATOR_ONBOARDING_SECRET="$(curl -sfL -X POST "$ONBOARDING_SECRET_URL")"
+    echo "Fetched user onboarding secret: $APP_USER_VALIDATOR_ONBOARDING_SECRET"
 fi
 
 exec /app/entrypoint.sh "$@"
