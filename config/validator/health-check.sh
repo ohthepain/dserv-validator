f8c7ba24cafeb196093a73ec4f6bbd26d5881274
Paul Wilkinson <cremoni@gmail.com>
2025-10-29 09:10:40 +0100

    hard code health check ports

--- config/validator/health-check.sh
+++ config/validator/health-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-curl -f http://localhost:2${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz
-curl -f http://localhost:3${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz
+curl -f http://localhost:25003/api/validator/readyz
+curl -f http://localhost:35003/api/validator/readyz
