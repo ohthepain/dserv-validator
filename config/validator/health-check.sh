f84e431a869c191a1502896356621bd74c89745f
Paul Wilkinson <cremoni@gmail.com>
2025-10-29 09:07:31 +0100

    validator health check

--- config/validator/health-check.sh
+++ config/validator/health-check.sh
@@ -3,9 +3,3 @@ set -e
 
 curl -f http://localhost:2${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz
 curl -f http://localhost:3${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz
-if [ "$LOCALNET_ENABLED" = true ]; then
-  curl -f http://localhost:4${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz
-  curl -f http://localhost:5012/api/scan/readyz
-  curl -f http://localhost:5014/api/sv/readyz
-fi
-
